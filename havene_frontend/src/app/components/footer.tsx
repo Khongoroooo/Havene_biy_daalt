ee76991393155ded6d01406859a6d677c2351233
Hongorzul <hongoroo1289810@gmail.com>
2025-10-02 15:46:09 +0800

    footer

--- havene_frontend/src/app/components/footer.tsx
+++ havene_frontend/src/app/components/footer.tsx
@@ -1,8 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <>
-      <div className="bg-amber-500"></div>
-    </>
+    <footer className="relative bg-[#F6F2EB] w-full h-52 flex flex-col justify-center">
+      {/* Logo зүүн талд */}
+      <div className="absolute top-1/2 left-16 -translate-y-1/2">
+        <Link href="/">
+          <Image
+            src="/haveneLogo.png"
+            alt="site logo"
+            width={105}
+            height={89}
+          />
+        </Link>
+      </div>
+
+      {/* Footer content center */}
+      <div className="flex flex-col items-center mt-32 space-y-2">
+        <p className="text-gray-700">© 2025 Havene. All rights reserved.</p>
+        <div className="flex space-x-6 text-gray-700">
+          <p>📧 info@havene.mn</p>
+          <p>📞 +976 9911 2233</p>
+          <p>🏢 Ulaanbaatar, Mongolia</p>
+        </div>
+        {/* <div className="flex space-x-4 mt-2">
+          <Link href="#">
+            <span className="hover:text-blue-600">Facebook</span>
+          </Link>
+          <Link href="#">
+            <span className="hover:text-blue-400">Twitter</span>
+          </Link>
+          <Link href="#">
+            <span className="hover:text-pink-500">Instagram</span>
+          </Link>
+        </div> */}
+      </div>
+    </footer>
   );
 };
+
 export default Footer;
--- havene_frontend/src/app/page.tsx
+++ havene_frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Home from "./home";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <Home />
-      
+      <Footer />
     </>
   );
 }
